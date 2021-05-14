1459e9274dc695d01b27e0f425bd1aedc09b9413
ArV <aryanrajvaidya@gmail.com>
2021-05-15 04:13:28 +0530

    Total Redesign of Time-Table Screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 const Color kBackgroundColor = Color(0xFF2F3A4F);
 const Color kElevatedBackgroundColor = Color(0xFF595C6E);
 const Color kRedAccentColor = Color(0xFFDB2420);
-const Color kYellowAccentColor = Color(0xFFF7C202);
+const Color kYellowAccentColor = Color(0xFFF5E960);
 const Color kLightTextColor = Color(0xFFB8BAC6);
 const Color kHintLightTextColor = Color(0xFF8E91A4);
+const Color kLightAccentColor = Color(0xFF9AA0A8);
--- lib/screens/time_table.dart
+++ lib/screens/time_table.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:pec_student/constants.dart';
-import 'package:pec_student/screens/edit_time_table.dart';
 import 'package:pec_student/services/networking.dart';
 
 class TimeTable extends StatefulWidget {
@@ -14,132 +13,249 @@ class TimeTable extends StatefulWidget {
 }
 
 class _TimeTableState extends State<TimeTable> {
-  String branch;
-  int year;
-  int difference = 0;
-  int weekday = DateTime.now().weekday - 1;
-  Map timeTable;
-  List<Widget> editTimeTable = [];
-  Widget timeTableList = Text('Loading');
+  Map timeTable = {};
+  int selectedDay = 0;
+  List<Widget> classCards = [Text('Loading...')];
   List<String> weekdays = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
   ];
+  void buildClassCards() {
+    List<Widget> cards = [];
+    String day = weekdays[selectedDay];
+    Map timeTableForTheDay = timeTable[day];
+    print(timeTableForTheDay);
+    if (timeTableForTheDay.length == 0) {
+      print('here');
+      setState(() {
+        classCards = [
+          SizedBox(
+            height: 200.0,
+          ),
+          Container(
+              margin: EdgeInsets.only(left: 10.0),
+              child: Text(
+                'No Classes Scheduled For Today',
+                style: TextStyle(color: kLightAccentColor, fontSize: 60.0),
+              ))
+        ];
+      });
+      return;
+    }
+    for (String time in timeTableForTheDay.keys) {
+      int hours = int.parse(time.substring(0, 2));
+      if (hours > 12) {
+        hours -= 12;
+      }
+      String h = hours.toString();
+      if (hours < 10) {
+        h = '0' + h;
+      }
+
+      String m = (time.substring(2));
+
+      cards.add(
+        classCardBuilder(
+          subject: timeTableForTheDay[time]['name'],
+          time: h + ':' + m,
+          classLink: timeTableForTheDay[time]['link'],
+          duration: timeTableForTheDay[time]['duration'],
+          groups: timeTableForTheDay[time]['groups'],
+        ),
+      );
+    }
+    setState(() {
+      classCards = [];
+      for (int i = cards.length - 1; i >= 0; i--) {
+        classCards.add(cards[i]);
+      }
+    });
+  }
 
   void getTimeTable() async {
     timeTable = await Networking().getTimeTable();
-    setState(() {
-      timeTableList = buildTimeTable();
-    });
-    if (await Networking().isAdmin()) {
-      setState(() {
-        editTimeTable.add(TextButton(
-          child: Text('Edit'),
-          onPressed: () {
-            Navigator.pushNamed(context, EditTimeTable.id);
-          },
-        ));
-      });
-    }
-  }
-
-  Widget buildTimeTable() {
-    Map tt = timeTable[weekdays[weekday]];
-    print(tt);
-    List<Widget> timeTableCards = [];
-    if (tt.length == 0) {
-      print('here');
-      return Text('No Classes Today');
-    }
-
-    for (String k in tt.keys) {
-      Widget card = Container(
-        height: 64.0,
-        decoration: BoxDecoration(
-            color: kElevatedBackgroundColor,
-            borderRadius: BorderRadius.circular(10.0)),
-        margin: EdgeInsets.symmetric(vertical: 32.0, horizontal: 32.0),
-        padding: EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(k),
-            ),
-            Text(tt[k])
-          ],
-        ),
-      );
-      timeTableCards.add(card);
-    }
-    return ListView(
-      children: timeTableCards,
-    );
+    buildClassCards();
   }
 
   @override
   void initState() {
-    // TODO: implement initState
-    if (weekday == -1) {
-      weekday = 7;
-    }
-    getTimeTable();
-
     super.initState();
+    getTimeTable();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kBackgroundColor,
-        centerTitle: true,
-        title: Text(
-          'Time Table',
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: kLightAccentColor,
+              width: 80.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 100.0,
+                  ),
+                  buildGestureDetector(index: 0, day: 'S'),
+                  buildGestureDetector(index: 1, day: 'M'),
+                  buildGestureDetector(index: 2, day: 'T'),
+                  buildGestureDetector(index: 3, day: 'W'),
+                  buildGestureDetector(index: 4, day: 'T'),
+                  buildGestureDetector(index: 5, day: 'F'),
+                  buildGestureDetector(index: 6, day: 'S'),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: kBackgroundColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 100.0,
+                      margin: EdgeInsets.only(left: 10.0, right: 20.0),
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          'Time Table',
+                          style: TextStyle(
+                            color: kLightAccentColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 10.0, right: 20.0),
+                      child: Divider(
+                        thickness: 5.0,
+                        color: kLightAccentColor,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: classCards,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-        actions: editTimeTable,
       ),
-      body: Column(
+    );
+  }
+
+  Container classCardBuilder(
+      {@required String subject,
+      @required String time,
+      @required String classLink,
+      @required String duration,
+      @required String groups}) {
+    return Container(
+      height: 150,
+      margin: EdgeInsets.only(left: 10.0, right: 20.0, top: 30.0),
+      child: Row(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  onPressed: () {
-                    setState(() {
-                      weekday--;
-                      if (weekday == -1) {
-                        weekday = 7;
-                      }
-                      timeTableList = buildTimeTable();
-                    });
-                  }),
-              Text(weekdays[weekday]),
-              IconButton(
-                  icon: Icon(Icons.arrow_forward_ios),
-                  onPressed: () {
-                    setState(() {
-                      weekday++;
-                      if (weekday == 7) {
-                        weekday = 0;
-                      }
-                      timeTableList = buildTimeTable();
-                    });
-                  }),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                time,
+                style: TextStyle(
+                  color: Color(0xFFC4C4C4),
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                duration + ' hr',
+                style: TextStyle(
+                  color: kLightAccentColor,
+                  fontSize: 20.0,
+                ),
+              ),
             ],
           ),
           Expanded(
-            child: timeTableList,
-          ),
+            child: Container(
+              margin: EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                  color: kLightAccentColor,
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Text(
+                          subject,
+                          style: TextStyle(
+                              color: kYellowAccentColor, fontSize: 25.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        'For Group(s) : ' + groups,
+                        style:
+                            TextStyle(color: kBackgroundColor, fontSize: 15.0),
+                      )),
+                ],
+              ),
+            ),
+          )
         ],
       ),
+    );
+  }
+
+  GestureDetector buildGestureDetector(
+      {@required int index, @required String day}) {
+    return GestureDetector(
+      child: Container(
+        height: 70.0,
+        width: 70.0,
+        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          color: (selectedDay == index) ? kYellowAccentColor : kBackgroundColor,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Center(
+          child: Text(
+            day,
+            style: TextStyle(
+              color:
+                  (selectedDay == index) ? kBackgroundColor : kLightAccentColor,
+              fontSize: 35.0,
+            ),
+          ),
+        ),
+      ),
+      onTap: () {
+        setState(() {
+          selectedDay = index;
+          buildClassCards();
+        });
+      },
     );
   }
 }
